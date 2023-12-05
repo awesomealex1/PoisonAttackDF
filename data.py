c3666af6d2d25100691830e9c0873965fbc29207
Alexander Murphy <lordpizza11@gmail.com>
2023-12-05 18:07:39 +0000

    .

--- data.py
+++ data.py
@@ -10,9 +10,6 @@ def get_image_from_video(video_path):
     except OSError: 
         print ('Error: Creating directory of tmp image data') 
     
-    print("A",os.path.isfile(video_path))
-    print("B",cam.isOpened())
-
     currentframe = 0
     frame_to_get = 100
 
